4222d4abe855f996bf21c224cdc637df0d10e1e9
Мария <kolesnikovamaria169@gmail,com>
2023-11-19 09:47:40 +0200

    Initial commit

--- src/test/java/ru/netology/statistic/StatisticTest.java
+++ src/test/java/ru/netology/statistic/StatisticTest.java
@@ -16,5 +16,17 @@ public class StatisticTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    @Test
+    void findMax1() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {6, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 
 }
